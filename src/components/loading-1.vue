05633e497f76c7cad67eaf38c113bfb47c0aa310
Toffee <toffee24@foxmail.com>
2018-05-10 14:31:45 +0800

    Initial commit

---
+++ src/components/loading-1.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="loading">
+    <div class="ivu-spin-main"><span class="ivu-spin-dot"></span> <div class="ivu-spin-text">加载中</div></div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      title: {
+        type: String,
+        default: '正在载入...'
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+  @keyframes ani-spin-bounce {
+    0% {
+      -webkit-transform: scale(0);
+      transform: scale(0)
+    }
+
+    to {
+      -webkit-transform: scale(1);
+      transform: scale(1);
+      opacity: 0
+    }
+  }
+  .loading{
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    text-align: center;
+    z-index: 1000;
+    background-color: hsla(0,0%,100%,.9);
+    .ivu-spin-main{
+      position: absolute;
+      top: 42%;
+      left: 50%;
+      -webkit-transform: translate(-50%,-50%);
+      transform: translate(-50%,-50%);
+      .ivu-spin-dot{
+        width: .32rem;
+        height: .32rem;
+        position: relative;
+        display: inline-block;
+        border-radius: 50%;
+        background-color: #2d8cf0;
+        -webkit-animation: ani-spin-bounce 1s 0s ease-in-out infinite;
+        animation: ani-spin-bounce 1s 0s ease-in-out infinite;
+      }
+      .ivu-spin-text{
+        margin-top: -.2rem;
+        color: #2d8cf0;
+        font-size: .14rem;
+      }
+    }
+  }
+</style>
---
+++ src/utils/helper.js
@@ -0,0 +1,79 @@
+export function addClass(el, className) {
+  if (hasClass(el, className)) {
+    return
+  }
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass.join(' ')
+}
+
+export function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
+}
+
+export function getData(el, name, val) {
+  let prefix = 'data-'
+  name = prefix + name
+  if (val) {
+    return el.setAttribute(name, val)
+  } else {
+    return el.getAttribute(name)
+  }
+}
+
+let elementStyle = document.createElement('div').style
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    MOZ: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== 'undefinded') {
+      return key
+    }
+  }
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+  if (vendor === 'standard') {
+    return style
+  }
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+//数组混乱
+export function shuffle(arr) {
+  let _arr = arr.slice()
+  for (let i = 0; i < _arr.length; i++) {
+    let j = getRandomInt(0, i)
+    let t = _arr[i]
+    _arr[i] = _arr[j]
+    _arr[j] = t
+  }
+  return _arr
+}
+
+//节流
+export function debounce(func, delay) {
+  let timer
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      func.apply(this, args)
+    }, delay)
+  }
+}
